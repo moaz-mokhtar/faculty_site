6624b19c463d2fdd2f82ee87512aa76ff0a65f9c
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2022-02-09 14:52:29 +0200

    Design Admin and About pages

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 extern crate env_logger;
 extern crate handlebars;
 
-use handlebars::Handlebars;
+use handlebars::{template, Handlebars};
 use log::info;
 use std::error::Error;
 use std::fs::File;
 use std::sync::Arc;
 use std::{collections::HashMap, env};
 use tiny_http::{Header, Response, Server};
+
+//TODO - using register_templates_directory
 
 fn main() -> Result<(), Box<dyn Error>> {
     initiate_logging();
@@ -60,6 +62,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn construct_data() -> Result<HashMap<String, String>, Box<dyn Error>> {
     let mut data: HashMap<String, String> = HashMap::new();
 
+    // templates mapping data
+    data.insert("app".to_string(), "app.hbs".to_string());
     data.insert("footer".to_string(), "footer.hbs".to_string());
     data.insert("header".to_string(), "header.hbs".to_string());
     data.insert("main".to_string(), "main.hbs".to_string());
@@ -70,12 +74,25 @@ fn construct_data() -> Result<HashMap<String, String>, Box<dyn Error>> {
         "carousel_images.hbs".to_string(),
     );
     data.insert("news".to_string(), "news.hbs".to_string());
+    data.insert("courses_card".to_string(), "courses_card.hbs".to_string());
+    data.insert("events_card".to_string(), "events_card.hbs".to_string());
+    data.insert("faculity_card".to_string(), "faculity_card.hbs".to_string());
+    data.insert("about".to_string(), "about.hbs".to_string());
+
+    // Logic data
+    data.insert("is_admin".to_string(), "true".to_string());
+
     dbg!(data.clone());
+
     Ok(data)
 }
 
 /// Register Handlebars templates
 fn register_templates() -> Result<Handlebars<'static>, Box<dyn Error>> {
+    let templates_dir =
+        std::env::var("TEMPLATES_DIR").expect("Missed 'TEMPLATES_DIR' environment variable");
+    info!("TEMPLATES_DIR dir: {}", templates_dir);
+
     let base_templates =
         std::env::var("BASE_TEMPLATES").expect("Missed 'BASE_TEMPLATES' environment variable");
     info!("BASE_TEMPLATES dir: {}", base_templates);
@@ -111,6 +128,19 @@ fn register_templates() -> Result<Handlebars<'static>, Box<dyn Error>> {
             parts_templates.clone() + "carousel_images.hbs",
         ),
         ("news".to_string(), parts_templates.clone() + "news.hbs"),
+        (
+            "courses_card".to_string(),
+            parts_templates.clone() + "courses_card.hbs",
+        ),
+        (
+            "events_card".to_string(),
+            parts_templates.clone() + "events_card.hbs",
+        ),
+        (
+            "faculity_card".to_string(),
+            parts_templates.clone() + "faculity_card.hbs",
+        ),
+        ("about".to_string(), parts_templates.clone() + "about.hbs"),
     ]);
 
     // register templates in handlebars
@@ -118,6 +148,19 @@ fn register_templates() -> Result<Handlebars<'static>, Box<dyn Error>> {
         handlebars.register_template_file(key, path).unwrap();
         info!("`{}` template resgistered, path: {}", key, path);
     });
+
+    // // register templates by directory
+    // handlebars
+    //     .register_templates_directory(".hbs", templates_dir)
+    //     .unwrap();
+    // let templates = handlebars.get_templates();
+    // info!("registered templates count: {}", templates.capacity());
+    // templates.iter().for_each(|(_name, _template)| {
+    //     //=
+    //     info!("=> template name: {}", _name);
+    // });
+
+    // dbg!(templates);
 
     Ok(handlebars)
 }
